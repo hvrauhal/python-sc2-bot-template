e0302b598c5e9327cf03527967536ebdbedb16cd
Heikki Rauhala <heikki.rauhala@reaktor.fi>
2018-06-08 10:40:41 +0300

    Run only against Zerg or Protoss

--- run_locally.py
+++ run_locally.py
@@ -4,6 +4,7 @@ from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
 
 from bot import MyBot
+from random import randrange
 
 def main():
     with open("botinfo.json") as f:
@@ -11,9 +12,10 @@ def main():
 
     race = Race[info["race"]]
 
+    enemy_race = Race.Zerg if randrange(0,2) == 0 else Race.Protoss
     run_game(maps.get("Abyssal Reef LE"), [
         Bot(race, MyBot()),
-        Computer(Race.Random, Difficulty.Medium)
+        Computer(enemy_race, Difficulty.Medium)
     ], realtime=False, step_time_limit=2.0, game_time_limit=(60*20), save_replay_as="test.SC2Replay")
 
 if __name__ == '__main__':

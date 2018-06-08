2585af3dbccd784fea20b8118fc6ea968403a52e
Jyri Kytömäki <jyri.kytomaki@reaktor.com>
2018-06-08 13:11:26 +0300

    Harder AI

--- run_locally.py
+++ run_locally.py
@@ -15,7 +15,7 @@ def main():
     enemy_race = Race.Zerg if randrange(0,2) == 0 else Race.Protoss
     run_game(maps.get("Abyssal Reef LE"), [
         Bot(race, MyBot()),
-        Computer(enemy_race, Difficulty.Medium)
+        Computer(enemy_race, Difficulty.Hard)
     ], realtime=False, step_time_limit=2.0, game_time_limit=(60*20), save_replay_as="test.SC2Replay")
 
 if __name__ == '__main__':
